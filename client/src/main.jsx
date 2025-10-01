50105024bb64aaeae9d063e37892ad4ac9e2df73
kamaludyn <kamaluddeen305@gmail.com>
2025-10-01 21:31:30 +0100

    feat: install and configure @acrool/react-toaster globally

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ToasterPortal, themeMap } from "@acrool/react-toaster";
+import "@acrool/react-toaster/dist/index.css";
+// import CustomCheckIcon from "../../../assets/custom_check.svg?react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <ToasterPortal
+      defaultTimeout={5000} // auto dismiss after 3s
+      limit={6} // max number of toasts at once
+      position={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      themeMap={themeMap} // you can customize colors/icons if needed
+    />
+  </StrictMode>
+);
