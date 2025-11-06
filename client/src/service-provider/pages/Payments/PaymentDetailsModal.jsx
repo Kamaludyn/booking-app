6fd767b4ae1994993e95e9304c29d0eeed87a9f7
kamaludyn <kamaluddeen305@gmail.com>
2025-11-06 20:01:42 +0100

    feat(Payments-ui): add detailsModal to display more payment details and copy transaction id functionality

--- client/src/service-provider/pages/Payments/PaymentDetailsModal.jsx
+++ client/src/service-provider/pages/Payments/PaymentDetailsModal.jsx
@@ -7,8 +7,8 @@ const PaymentDetailsModal = ({
   formatDate,
 }) => {
   return (
-    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-      <div className="bg-surface-600 w-full max-w-md rounded-2xl p-6 shadow-lg relative">
+    <div className="fixed inset-0 w-5/6 md:w-auto bg-black/50 flex items-center justify-center z-50">
+      <div className="bg-surface-500 dark:bg-surface-800 border border-border-500 dark:border-border-800 text-text-500 dark:text-white  w-full max-w-md rounded-2xl p-6 shadow-lg relative">
         <h3 className="text-lg font-semibold text-text-900 mb-4">
           Payment Details
         </h3>
@@ -26,14 +26,29 @@ const PaymentDetailsModal = ({
                 : selectedPayment.amountPaid
             }`}
           />
-          <Detail label="Payment Method" value={selectedPayment.method} />
-          <Detail label="Status" value={selectedPayment.status} />
           <Detail
-            label="Date"
+            label="Currency"
+            value={selectedPayment.currency.toUpperCase()}
+          />
+          <Detail label="Status" value={selectedPayment.status} />
+          <Detail label="Provider" value={selectedPayment.provider} />
+          <Detail label="Payment Method" value={selectedPayment.method} />
+          <Detail
+            label="Date/Time"
             value={`${formatDate(selectedPayment.createdAt).time} ${
               formatDate(selectedPayment.createdAt).date
             }`}
           />
+          <Detail label="Service" value={selectedPayment.serviceId.name} />
+          <Detail
+            label="Service Price"
+            value={`$${selectedPayment.serviceId.price}`}
+          />
+          <Detail
+            label="Booking Status"
+            value={selectedPayment.bookingId.status}
+          />
+          <Detail label="Booking Date" value={selectedPayment.bookingId.date} />
           <Detail
             label="Transaction ID"
             value={selectedPayment.providerSessionId.toUpperCase()}
@@ -63,9 +78,9 @@ const PaymentDetailsModal = ({
 
 function Detail({ label, value }) {
   return (
-    <div className="flex justify-between">
-      <span className="font-medium text-text-500">{label}</span>
-      <span className="text-text-900">{value}</span>
+    <div className="flex justify-between border-b border-border-500 dark:border-border-800 text-text-500 dark:text-white">
+      <span className="w-1/2 font-medium">{label}</span>
+      <span className="w-1/2 text-right word-break: break-all">{value}</span>
     </div>
   );
 }
